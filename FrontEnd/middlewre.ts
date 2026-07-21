6661668aa291ad36e1fdc74ed7f89aa188afb3d3
حمیدرضا فلاحی <h.falahi@ArianSystemdp.local>
2026-07-21 10:52:42 +0330

    add certbot renew and reload for nginx

--- FrontEnd/middlewre.ts
+++ FrontEnd/middlewre.ts
@@ -4,7 +4,8 @@ import createMiddleware from 'next-intl/middleware';
 export default createMiddleware({
   locales: ['fa', 'en'],
   defaultLocale: 'fa',
-  localePrefix: 'as-needed' 
+  localePrefix: 'as-needed' ,
+  localeDetection: true
 });
 
 export const config = {
