d96770ae81aec62b84d1229180a6d5ed413cde44
حمیدرضا فلاحی <h.falahi@ArianSystemdp.local>
2026-07-28 09:32:54 +0330

    remove api from first part

--- FrontEnd/lib/api.ts
+++ FrontEnd/lib/api.ts
@@ -2,7 +2,7 @@ const isDevelopment = process.env.NODE_ENV === "development";
 
 export const apiBaseUrl = isDevelopment
   ? process.env.NEXT_PUBLIC_INTERNAL_API_URL
-  : "/api";
+  : "";
 export const apiBaseServerSideUrl = isDevelopment
   ? process.env.INTERNAL_SERVER_SIDE_API_URL
   :  "";
